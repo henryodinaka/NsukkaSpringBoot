9f15fb1ce0bfd3ea22cc1a8b116dd50a8c19746c
UPPERLINK <you@example.com>
2019-02-27 23:59:24 +0100

    I modified UserController and the app is working up till now

--- src/main/java/nsk/cath/com/controller/UserController.java
+++ src/main/java/nsk/cath/com/controller/UserController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/user")
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/user")
+@Consumes(value = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+@Produces(value = {MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
 public class UserController {
 
-    @PostMapping
+    @POST
     public ResponseEntity<?> create(@RequestBody UserRequest userRequest)
     {
         return ResponseEntity.status(200).body("");
